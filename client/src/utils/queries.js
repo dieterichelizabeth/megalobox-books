64d75b89687b5d195b64ba9e639326a793b78533
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-07 23:53:42 -0500

    Updates client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,11 +2,19 @@ import { gql } from "@apollo/client";
 
 // Create GET_ME query
 export const GET_ME = gql`
-  {
+  query Query {
     me {
       _id
       username
       email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
